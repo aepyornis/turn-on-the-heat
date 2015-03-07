d6d9efe4f3153814e2a7c6bb87aa78ab2c34fcdf
aepyornis <ziggy@elephant-bird.net>
2015-03-07 18:29:41 -0500

    map w/ complaints from past week

--- public/js/map.js
+++ public/js/map.js
@@ -6,3 +6,16 @@ var map = L.map('map', {
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+complaints.forEach(add_circle_to_map);
+
+function add_circle_to_map(complaint) {
+  if (complaint.latitude && complaint.latitude) {
+    var lat_lng = [complaint.latitude, complaint.longitude];
+    var path_options = {
+      radius: 5
+    }
+    L.circleMarker(lat_lng, path_options).addTo(map);
+  }
+}
+
