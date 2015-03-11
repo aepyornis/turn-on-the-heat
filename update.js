eab89c035c20dc54e8d1b0a9305e3ba95a4ec26e
Ziggy <ziggy@elephant-bird.net>
2015-03-10 20:57:13 -0400

    download_complaints() works

--- update.js
+++ update.js
@@ -2,36 +2,35 @@ var request = require('request');
 var fs = require('fs');
 require('date-utils');
 
-// Get yesterday's complaints -> count them -> update json -> 
-// write complaints to file
+download_complaints(Date.yesterday().toFormat('YYYY-MM-DD'), function(arr){
+    console.log(arr.length);
+});
 
-// update map. 
-
-get_yesterdays_complaints();
-
-function get_yesterdays_complaints() {
+// input: 'YYYY-MM-DD' -> callback (arr of complaints)
+// output: callback (arr of complaints)
+// downloads yesterdays complaints, writes them to file
+function download_complaints(date, callback) {
 
     var url_path = "https://data.cityofnewyork.us/resource/erm2-nwe9.json?$select=unique_key, created_date, closed_date, descriptor, incident_zip, incident_address, city, borough, status, latitude, longitude &$WHERE=agency='HPD' AND created_date >= " 
-        url_path += "'" + Date.yesterday().toFormat('YYYY-MM-DD') + "T00:00:00' AND complaint_type = 'HEAT/HOT WATER' &$order=created_date DESC &$limit=10000";
+        url_path += "'" + date + "T00:00:00' AND complaint_type = 'HEAT/HOT WATER' &$order=created_date DESC &$limit=10000";
+    var file_path = date + '.txt';
     
-    var file_path = Date.yesterday().toFormat('YYYY-MM-DD') + '.json';
+    // streams
     var writeStream = fs.createWriteStream(file_path);
+    var requestStream = request(encodeURI(url_path));
 
-    request(encodeURI(url_path))
-        .on('response', function(response){
-            console.log('status code:' + response.statusCode)
-        })
-        .on('data', function(data){
-            // console.log(data.toString());
-        })
-        .pipe(writeStream);
-}
+    var string_data = '';
+    
+    requestStream.pipe(writeStream);
 
-function write_complaints_file() {
+    requestStream.on('data', function(data){
+        string_data += data.toString();
+    })
 
-}
-
-function count_comlaints() {
+    requestStream.on('end', function(){
+        var arr = JSON.parse(string_data);
+        callback(arr);
+    })
 
 }
 
@@ -41,11 +40,9 @@ function update_json () {
 
 
 
-
-
-
-
-
+// Get yesterday's complaints -> count them -> update json -> 
+// write complaints to file
+// update map. 
 
 
 
