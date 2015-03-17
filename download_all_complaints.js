f71968e65c46532cba56036f883106964331807d
Ziggy <ziggy@elephant-bird.net>
2015-03-16 21:40:31 -0400

    geocode, bulk-download

---
+++ download_all_complaints.js
@@ -0,0 +1,49 @@
+// downloads data from NYC open data, adds bbl field via geoclient, and then creates SQL file.
+require('date-utils');
+
+
+// string -> [string]
+// returns array of dates in 'YYYY-MM-DD' from a certain date until today. 
+function days_since(date) {
+    var date_array = [];    
+    var start = new Date(date);
+    var today = Date.today();
+    var days = start.getDaysBetween(today);
+
+    for (var i = 1; i < days; i++) {
+        var date = new Date(start.getFullYear(), start.getMonth(), (start.getDate() + i))
+
+        date_array.push(date.toFormat('YYYY-MM-DD'));
+    }   
+
+    return date_array;
+}
+
+
+// input: 'YYYY-MM-DD' -> callback (arr of complaints)
+// output: callback (arr of complaints)
+// downloads complaints, writes them to file
+function download_complaints(date, callback) {
+
+    var url_path = "https://data.cityofnewyork.us/resource/erm2-nwe9.json?$select=unique_key, created_date, closed_date, descriptor, incident_zip, incident_address, city, borough, status, latitude, longitude &$WHERE=agency='HPD' AND created_date >= " 
+        url_path += "'" + date + "' AND complaint_type = 'HEAT/HOT WATER' &$order=created_date DESC &$limit=50000";
+    var file_path = './data/' + date + '.txt';
+    
+    // streams
+    var writeStream = fs.createWriteStream(file_path);
+    var requestStream = request(encodeURI(url_path));
+
+    var string_data = '';
+    
+    requestStream.pipe(writeStream);
+
+    requestStream.on('data', function(data){
+        string_data += data.toString();
+    })
+
+    requestStream.on('end', function(){
+        var arr = JSON.parse(string_data);
+        callback(arr);
+    })
+
+}
